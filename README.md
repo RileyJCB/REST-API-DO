0f415a1f4a7d1c398b18838e28118a09a2697ea7
RileyJCB <59400909+RileyJCB@users.noreply.github.com>
2020-01-03 10:34:58 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -5,27 +5,20 @@
 ```
 requirements.txt
 #################
-aniso8601==8.0.0
-Click==7.0
-Flask==1.1.1
-Flask-JWT==0.3.2
-Flask-RESTful==0.3.7
-Flask-SQLAlchemy==2.4.1
-itsdangerous==1.1.0
-Jinja2==2.10.3
-MarkupSafe==1.1.1
-PyJWT==1.4.2
-pytz==2019.3
-six==1.13.0
-SQLAlchemy==1.3.12
-Werkzeug==0.16.0
+Flask
+Flask-JWT-Extended
+Flask-RESTful
+Flask-SQLAlchemy
+psycopg2
+uWSGI
+
 
 python app.py
 ```
 
 ## Description
 This is a test application from Udemy : REST APIs with Flask and Python.
-It has all the needed modules for deployment onto Heroku.
+It has all the needed modules for deployment onto Digital Ocean.
 
 ## Implementation
 This project is implemented using Flask and is a REST API for a store
--- app.py
+++ app.py
@@ -5,16 +5,17 @@ import os
 
 from datetime import timedelta
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 import uuid
 
-from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User, UserLogin, UserList, UserLogout, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.carter import Carter
+
+from blacklist import BLACKLIST
 
 # Resource(s) are just things, items, they are often mapped into database tables
 
@@ -28,11 +29,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:/
 # Turn of Flasks Database Tracker but SQLALCHEMY is still on
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# config JWT to expire within 3 hours
-#app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=10800)
-
 # Pass the actual error code to pass through flask and be communicated 
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+# Setup a BLACKLIST using JWT
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+# config JWT to expire within 3 hours
+#app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=10800)
 
 '''
 # config JWT auth key name to be 'email' instead of default 'username'
@@ -48,8 +53,60 @@ jwt = JWT(app, authenticate, identity)
 '''
 
 # This will set the auth enpoint to /login  to verify user
-app.config['JWT_AUTH_URL_RULE'] = '/login'
-jwt = JWT(app, authenticate, identity)
+#app.config['JWT_AUTH_URL_RULE'] = '/login'
+jwt = JWTManager(app) # This is not creating the /auth endpoint
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1: # Instead of hard-coding, you should read from a config file or a database
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+#    return decrypted_token['identity'] in BLACKLIST # Now using JTI vs user identity
+    return decrypted_token['jti'] in BLACKLIST
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+# Called when bad token string sent
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+# Call when a JWT is not sent at all
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+# This handles the case when we require a fresh token but then did not sent one
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+# Sets the token as invalid, this would handle a logout
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.',
+        'error': 'token_revoked'
+    }), 401
 
 # Makes it easy to control interface to Resource
 api = Api(app)
@@ -70,6 +127,11 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(Carter, '/games')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserList, '/users')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     from db import db
