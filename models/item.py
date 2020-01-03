797344fa3c570d3d3a3b4dcb6cf8197c11bf75ab
RileyJCB <59400909+RileyJCB@users.noreply.github.com>
2020-01-03 10:35:33 -0800

    Add files via upload

--- models/item.py
+++ models/item.py
@@ -14,7 +14,6 @@ class ItemModel(db.Model):
     store = db.relationship('StoreModel')
     # Also do a back reference in StoreModel
 
-
     def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
@@ -31,21 +30,6 @@ class ItemModel(db.Model):
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
-        #return ItemModel.query.filter_by(name=name).first()
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM items WHERE name=?"
-        print(query, name)
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-        if row: # All of the below are the same...
-            #return {'item': {'name': row[0], 'price': row[1]}}
-            #return cls(row[0], row[1])
-            return cls(*row)
-        return None
-        '''
 
     @classmethod
     def find_all(cls):
@@ -55,27 +39,7 @@ class ItemModel(db.Model):
 #    def insert_item(self):
         db.session.add(self)
         db.session.commit()
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "INSERT INTO items VALUES (?, ?)"
-        print("insert_item({}, {})".format(self.name, self.price))
-        cursor.execute(query, (self.name, self.price))
-        connection.commit()
-        connection.close()
-        '''
 
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-    '''
-    def update_item(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (self.price, self.name))
-
-        connection.commit()
-        connection.close()
-    '''
--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 import sqlite3
 from db import db
+from flask_jwt_extended import get_jwt_claims
 
 class UserModel(db.Model):
     __tablename__ = 'users'
@@ -12,46 +13,31 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
+    def json(self):
+        return {
+            'id': self.id,
+            'username': self.username
+        }
+
     def save_to_db(self):
         db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
         db.session.commit()
 
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,)) # Force a tuple with the extra ,
-        row = result.fetchone()
-        if row:
-            #user = cls(row[0], row[1], row[2]) # Same as below
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
-        '''
 
     @classmethod
     def find_by_id(cls, _id):
-        return cls.query.filter_by(id=_id)
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(id=_id).first()
 
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,)) # Force a tuple with the extra ,
-        row = result.fetchone()
-        if row:
-            #user = cls(row[0], row[1], row[2]) # Same as below
-            user = cls(*row)
-        else:
-            user = None
-
-        connection.close()
-        return user
-        '''
+    @classmethod
+    def find_all(cls):
+      #claims = get_jwt_claims()
+      #if not claims['is_admin']:
+      #  return {'message': 'Admin privilege is required.'}, 401
+      return cls.query.all()
