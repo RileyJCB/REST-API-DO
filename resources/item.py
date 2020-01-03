09e38f80c2bb68b752e62adee10bae4176b36116
RileyJCB <59400909+RileyJCB@users.noreply.github.com>
2020-01-03 10:35:59 -0800

    Add files via upload

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,12 @@
 
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import (
+    jwt_required, 
+    get_jwt_claims, 
+    jwt_optional, 
+    get_jwt_identity,
+    fresh_jwt_required
+)
 from models.item import ItemModel
 
 # The api works with resources and every resource has to be a class
@@ -18,13 +24,14 @@ class Item(Resource):
       help="Every item must be assigned to a store id!"
     )
 
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
             return item.json()
         return {'message': 'Item {} not found'.format(name)}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': "An item with name '{}' already exists".format(name)}, 400
@@ -40,25 +47,15 @@ class Item(Resource):
 
         return item.json(), 201
 
-    @jwt_required()
+    @jwt_required
     def delete(self, name):
+        claims = get_jwt_claims()
+        if not claims['is_admin']:
+            return {'message': 'Admin privilege is required.'}, 401
+
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-
-        '''
-        if not ItemModel.find_by_name(name):
-            return {'message': "No item by name '{}' exists".format(name)}, 400
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
-        '''
         return {'message': 'Item {} was deleted'.format(name)}, 201
 
     # PUT is an Idempotent method whick add_resource
@@ -76,36 +73,16 @@ class Item(Resource):
             item.price = data['price']
         item.save_to_db()
         return item.json()
-        '''
-        if ItemModel.find_by_name(name): # Update item
-            try:
-                item.update_item()
-                return {'message': 'update', 'item': item.json()}
-            except:
-                return {"message": "Error updating item {} in database.".format(name)}, 500  # Internal Server Error
-        else:                       # Create/add item
-            try:
-                item.insert_item()
-                return {'message': 'create', 'item': item.json()}
-            except:
-                return {"message": "Error adding item {} to database.".format(name)}, 500  # Internal Server Error
-        '''
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        # Pythonic approach with list comprehension
-        #return {'items': [item.json() for item in ItemModel.query.all()]}
-        return {'items': [item.json() for item in ItemModel.find_all()]}
-        # or using map and lambda if using Javascript may want to list map
-        # return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = result.fetchall()
-        connection.close()
-        if items:
-            return {'items': [{'name': n, 'price': p} for i, n, p in items]}
-        return {'message': 'No items in database!'}
-        '''
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'Login for more data.'
+            }, 200
+    
--- resources/user.py
+++ resources/user.py
@@ -1,35 +1,110 @@
-#import sqlite3
 from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (
+  create_access_token, 
+  create_refresh_token, 
+  jwt_required, 
+  get_jwt_claims, 
+  jwt_refresh_token_required,
+  get_jwt_identity,
+  jwt_required,
+  get_raw_jwt
+)
+
 from models.user import UserModel
+from blacklist import BLACKLIST
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument(
+  'username',
+  type=str,
+  required=True,
+  help="Username cannot be left blank!"
+)
+_user_parser.add_argument(
+  'password',
+  type=str,
+  required=True,
+  help="Password cannot be left blank!"
+)
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-      type=str,
-      required=True,
-      help="Username cannot be left blank!"
-    )
-    parser.add_argument('password',
-      type=str,
-      required=True,
-      help="Password cannot be left blank!"
-    )
-    def post(self):
-        data = UserRegister.parser.parse_args()
-        if UserModel.find_by_username(data['username']):
-            return {"message": "User {} already exists.".format(data['username'])}, 400
+  def post(self):
+    data = _user_parser.parse_args()
+    if UserModel.find_by_username(data['username']):
+        return {"message": "User {} already exists.".format(data['username'])}, 400
 
-        #user = UserModel(data['username'], data['password'])
-        user = UserModel(**data)
-        user.save_to_db()
-        '''
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+    #user = UserModel(data['username'], data['password'])
+    user = UserModel(**data)
+    user.save_to_db()
 
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
+    return {"message": "User {} create successfully.".format(data['username'])}, 201
 
-        connection.commit()
-        connection.close()
-        '''
-        return {"message": "User {} create successfully.".format(data['username'])}, 201
+class User(Resource):
+  @classmethod
+  def get(cls, user_id):
+    user = UserModel.find_by_id(user_id)
+    if not user:
+      return {'message': 'User not found'}, 404
+    return user.json()
+
+  @classmethod
+  def delete(cls, user_id):
+    user = UserModel.find_by_id(user_id)
+    if not user:
+      return {'message': 'User not found'}, 404
+
+    user.delete_from_db()
+    return {'message': 'User {} deleted.'.format(user.username)}
+
+
+class UserLogin(Resource):
+  @classmethod
+  def post(cls):
+    # get data from parser
+    data = _user_parser.parse_args()
+
+    # find user in database
+    user = UserModel.find_by_username(data['username'])
+
+    # check password
+    if user and safe_str_cmp(user.password, data['password']):
+      # create access token
+      access_token = create_access_token(identity=user.id, fresh=True)
+      # create refresh token
+      refresh_token = create_refresh_token(user.id)
+  
+      # return them
+      return {
+        'access_token': access_token,
+        'refresh_token': refresh_token
+      }, 200
+
+    return {'message': 'Invalid Username or Password.'}, 401
+
+
+class UserLogout(Resource):
+  @jwt_required
+  def post(self):
+    jti = get_raw_jwt()['jti'] # jit is "JWT ID", a unique identifier for that JWT
+    user = UserModel.find_by_id(get_jwt_identity()).json()['username']
+    BLACKLIST.add(jti)
+    return {'message': '{} has successfully logged out.'.format(user)}
+
+
+class UserList(Resource):
+  @jwt_required
+  def get(self):
+    claims = get_jwt_claims()
+    if not claims['is_admin']:
+      return {'message': 'Admin privilege is required.'}, 401
+
+    return {'users': [user.json() for user in UserModel.find_all()]}
+
+
+class TokenRefresh(Resource):
+  @jwt_refresh_token_required
+  def post(self):
+    current_user = get_jwt_identity()
+    new_token = create_access_token(identity=current_user, fresh=False)
+    return {'access_token': new_token}, 200
